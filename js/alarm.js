951c713cb187b481e450b1694a9d5e54dc5c85b5
mrlead <krendelek1509999@gmail.com>
2018-04-25 23:58:45 +0300

    Ещё изменения библиотек связанных со временем

--- js/alarm.js
+++ js/alarm.js
@@ -1,20 +1,42 @@
-<script type="text/javascript">
-function yours_function(){
-alert('Ваше сообщение');
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
 }
 
-function timer(){
-var obj=document.getElementById('time');
-obj.innerHTML--;
-if(obj.innerHTML==0) {
-    yours_function(); 
-    obj.innerHTML=10; 
-    setTimeout(timer,1000);
-}
-else{ setTimeout(timer,1000); }
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
 }
 
-setTimeout(timer,1000);
-</script>
-
-<span id="time">10</span>
+var deadline="January 01 2018 00:00:00 GMT+0300";
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
+initializeClock('clockdiv', deadline);
