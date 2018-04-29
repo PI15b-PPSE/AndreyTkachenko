7b097dd1fbaa42e18e50353a5196f1bf2a7b9e7e
mrlead <krendelek1509999@gmail.com>
2018-04-29 16:56:15 +0300

    Борьба с политикой безопаснсти, проблема в запуске функции из внешнего скрипта

---
+++ js/difficult.js
@@ -0,0 +1,17 @@
+function easy_func()
+{
+	window.onload = function()
+	{
+		document.getElementById('task_1').style.background-color = 'green';
+	}
+}
+
+function middle_func()
+{
+	document.getElementById("task_2").style.background-color = 'yellow';
+}
+
+function hard_func()
+{
+	document.getElementById("task_3").style.background-color = 'red';
+}
