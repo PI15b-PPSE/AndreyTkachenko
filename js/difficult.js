75b6dfcfc7b5e2132b972247e9b400fa0d1e1a85
mrlead <krendelek1509999@gmail.com>
2018-05-06 18:00:28 +0300

    Восстановление после конфликта

--- js/difficult.js
+++ js/difficult.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
     $('a').click(function(e){
         e.preventDefault();
-<<<<<<< HEAD
-		document.getElementById($(this).data('arg1')).style.backgroundColor = $(this).data('arg2');;
-=======
 		document.getElementById($(this).data('arg1')).style.backgroundColor = $(this).data('arg2');
 		var data_saver = {};
 		data_saver["elem"] = $(this).data('arg1');
 		data_saver["color"] = $(this).data('arg2');
 		chrome.storage.local.set(data_saver);
->>>>>>> lab3.2
 	});
 });
